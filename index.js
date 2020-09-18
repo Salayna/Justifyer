94ac70e9838ee56cc259b78474195ca38afca335
Salayna Doukoure <salayna@macbook-pro-de-salayna.home>
2020-09-18 15:02:20 +0200

    Tokenization of the mail

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express =  require('express');
 const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const jwtSecret = process.env.SECRET || 'DoesTheGoldGoblinWorthMoreThanTheCopperDragon';
 
 const app = express();
 const PORT =  process.env.PORT || 8080;
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome Stranger');
 })
-app.post('/api/justify', (req, res) => {
+    .post('/api/token', (req, res) => {
+        const mail = req.body.email;
+        const token = jwt.sign(mail, jwtSecret, {
+          expiresIn: '24h'
+        });
+        res.send({
+          token: token,
+        });
+})
+    .post('/api/justify', (req, res) => {
 
 })
 .listen(PORT, () => {
